293e43fecbe7f556cdf65c160389bd5c54b3ec58
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-02-12 17:27:03 +0500

    Playing with animation of SVGs

---
+++ 03_SVG-Animations/index.js
@@ -0,0 +1,25 @@
+let InitialPath = `M 10 100 Q 500 100 909 100`;
+let finalPath = `M 10 100 Q 500 100 909 100`;
+
+const string = document.querySelector("#string");
+string.addEventListener("mousemove", (dets) => {
+  InitialPath = `M 10 100 Q ${dets.x} ${dets.y} 909 100`;
+
+  gsap.to("svg path", {
+    attr: { d: InitialPath },
+    duration: 0.5,
+    ease: "power3.out",
+  });
+});
+
+string.addEventListener("mouseleave", () => {
+  gsap.to("svg path", {
+    attr: { d: finalPath },
+    duration: 2,
+    ease: "elastic.out(1, 0.2)",
+  });
+});
+
+// This format of backtick and dollar sign with braces is called template literals
+// const name = "Waqar";
+// console.log(`Hello, ${name}!`); // Output: Hello, Waqar!
