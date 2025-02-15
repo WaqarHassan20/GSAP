a5d6ace042c4734460573da5570730bc40dc056f
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-02-15 20:21:09 +0500

    Trying the new cursor animation

--- 04_CustomCursor/index.js
+++ 04_CustomCursor/index.js
@@ -2,12 +2,32 @@ const main = document.getElementById("main");
 
 const cursor = document.getElementById("cursor");
 
+const imageDiv = document.getElementById("image");
+
 main.addEventListener("mousemove", (dets) => {
   gsap.to(cursor, {
     x: dets.clientX,
     y: dets.clientY,
     duration: 1,
-    ease: "back.out(1.5)"
+    ease: "back.out(1.5)",
+  });
+});
+
+imageDiv.addEventListener("mouseenter", () => {
+  cursor.innerHTML = "Explore more";
+
+  gsap.to("#cursor", {
+    scale: 4,
+    backgroundColor: "#ffffffb6",
+  });
+});
+
+imageDiv.addEventListener("mouseleave", () => {
+  cursor.innerHTML = "";
+
+  gsap.to("#cursor", {
+    scale: 1,
+    backgroundColor: "#fff",
   });
 });
 
