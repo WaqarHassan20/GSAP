b2b8a33b5877c9b337601345bcd846c78e52ca67
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-02-21 20:11:54 +0500

    Text Animations in GSAP

--- 06_TextAnimations/index.js
+++ 06_TextAnimations/index.js
@@ -5,10 +5,17 @@ function BreakTheString() {
 
   let splitText = headingText.split("");
 
+  let halfValue = Math.floor(splitText.length / 2);
+  console.log(halfValue);
+
   let clutter = "";
 
-  splitText.forEach((num) => {
-    clutter += `<span>${num}</span>`;
+  splitText.forEach((el, index) => {
+    if (index < halfValue) {
+      clutter += `<span class="first" >${el}</span>`;
+    } else {
+      clutter += `<span class="last" >${el}</span>`;
+    }
   });
 
   heading.innerHTML = clutter;
@@ -17,3 +24,30 @@ function BreakTheString() {
 }
 
 BreakTheString();
+
+// gsap.from("h2 span", {
+//   y: 100,
+//   opacity: 0,
+//   duration: 0.8,
+//   delay: 0.5,
+//   // stagger: 0.1,
+//   // stagger: () => Math.random() - 0.5,
+//   // stagger: -0.15, this will reverse the animations
+//   // stagger: (char) => (char % 2 === 0 ? -0.5 : 0.5),
+// });
+
+gsap.from("h2 .first", {
+  y: 200,
+  delay: 0.8,
+  opacity: 0,
+  stagger: 0.12,
+  duration: 0.8,
+});
+
+gsap.from("h2 .last", {
+  y: 200,
+  delay: 0.8,
+  opacity: 0,
+  stagger: -0.12,
+  duration: 0.8,
+});
