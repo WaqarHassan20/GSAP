6fc72e1d5d2126d8325724b3ca7e1ae247cac503
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-03-02 19:51:51 +0500

    Learn how to play with SVG animations

--- 03_SVG-Animations/index.js
+++ 03_SVG-Animations/index.js
@@ -23,3 +23,28 @@ string.addEventListener("mouseleave", () => {
 // This format of backtick and dollar sign with braces is called template literals
 // const name = "Waqar";
 // console.log(`Hello, ${name}!`); // Output: Hello, Waqar!
+
+// // ===================================== //
+// // Self practiced the code of svg animation
+// // ===================================== //
+// let InitialPath = `M 10 100 Q 500 100 909 100`;
+// let finalPath = `M 10 100 Q 500 100 909 100`;
+
+// const string = document.querySelector("#string");
+// string.addEventListener("mousemove", (dets) => {
+//   InitialPath = `M 10 100 Q ${dets.x} ${dets.y} 909 100`;
+
+//   gsap.to("svg path", {
+//     attr: { d: InitialPath },
+//     duration: 0.5,
+//     ease: "power.out",
+//   });
+// });
+
+// string.addEventListener("mouseleave", () => {
+//   gsap.to("svg path", {
+//     attr: { d: finalPath },
+//     duration: 2,
+//     ease: "elastic.out(1,0.2)",
+//   });
+// });
