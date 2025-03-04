96fcace88ecd63bf3f002ebc527d1ac64edf7672
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-03-04 20:26:43 +0500

    How to use GSAP in React Project

--- 09_GSAP_in_React/src/App.jsx
+++ 09_GSAP_in_React/src/App.jsx
@@ -1,29 +1,11 @@
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
+import Index from "./components";
+import UseRefGSAP from "./components/UseRefGSAP";
 
 function App() {
-  useGSAP(() => {
-    gsap.to(".mydiv", {
-      x: 1000,
-      y: 200,
-      borderRadius: 100,
-      duration: 1,
-      scale: 1.5,
-      rotate: 180,
-      yoyo: true,
-      repeat: -1,
-      backgroundColor: "blue",
-      ease: "power1.inOut",
-    });
-  });
-
   return (
     <>
-      <main className="h-screen w-screen bg-black">
-        <div className="mydiv w-44 h-44 bg-red-600 rounded-2xl flex justify-center items-center text-white font-bold text-2xl">
-          I am Div
-        </div>
-      </main>
+        {/* <UseRefGSAP /> */}
+        <Index />
     </>
   );
 }
