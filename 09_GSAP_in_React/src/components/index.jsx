e8f1c9ef4e8ecb689305aba89f6b2ff66b105b2e
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-03-04 20:26:27 +0500

    How to use GSAP in React Project

---
+++ 09_GSAP_in_React/src/components/index.jsx
@@ -0,0 +1,20 @@
+function Index() {
+  return (
+    <>
+      <main>
+        <div className="border-2 border-white p-20">
+          <div className="border-2 border-orange-700 rounded-2xl">
+            <div className="w-40 h-40 bg-gradient-to-b from-blue-500 to-blue-700 rounded-2xl"></div>
+            <div className="w-40 h-40 bg-gradient-to-b from-yellow-500 to-yellow-700 rounded-full"></div>
+          </div>
+
+          <div>
+            <div className="w-40 h-40 bg-gradient-to-b from-blue-500 to-blue-700 rounded-2xl"></div>
+            <div className="w-40 h-40 bg-gradient-to-b from-yellow-500 to-yellow-700 rounded-full"></div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+export default Index;
