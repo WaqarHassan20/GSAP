0a8fc3ecea95b23f6dffbebbb62294178381b6ce
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-03-06 21:31:55 +0500

    How to use GSAP in React Project

--- 09_GSAP_in_React/src/App.jsx
+++ 09_GSAP_in_React/src/App.jsx
@@ -1,15 +1,17 @@
 import UseRefGSAP from "./components/UseRefGSAP";
-import UserRefGSAP2 from "./components/UseRefGSAP2";
-import ScopeInGSAP from "./components/ScopeInGSAP";
-import ButtonAnimate from "./components/ButtonAnimate";
+// import UserRefGSAP2 from "./components/UseRefGSAP2";
+// import ScopeInGSAP from "./components/ScopeInGSAP";
+// import ButtonAnimate from "./components/ButtonAnimate";
+
+// Use the one component at a time to better visulaize the effects of the animations of the GSAP
 
 function App() {
   return (
     <>
       <UseRefGSAP />
-      <UserRefGSAP2 />
-      <ScopeInGSAP />
-      <ButtonAnimate />
+      {/* <UserRefGSAP2 /> */}
+      {/* <ScopeInGSAP /> */}
+      {/* <ButtonAnimate /> */}
     </>
   );
 }
